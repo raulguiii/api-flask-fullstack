afa94c0b165b53725c0081be526c8c07e3794dd1
Raul Guilherme <143142847+raulguiii@users.noreply.github.com>
2024-09-17 15:02:27 -0300

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT 1,
+    cpf_cnpj TEXT NOT NULL
+);
